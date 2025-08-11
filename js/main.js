5911c605c3e9f253ebba5cca5f8f2c76df713af6
Gigashmaru <golyholy05@gmail.com>
2025-08-11 11:27:13 -0700

    Bootstrap

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ const navMenu = document.querySelector(".nav-menu");
 hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loading = document.getElementById("loading");
+
+  // Прелоадер исчезает через 2 секунды
+  const loadingDuration = 2000;
+  if (loading) {
+    setTimeout(() => {
+      loading.classList.add("loading-none");
+    }, loadingDuration);
+  }
+});
